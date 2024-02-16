549fbb44546840fd676485607f319c7bbe6e412b
vijaypython9210 <134040830+vijaypython9210@users.noreply.github.com>
2024-02-16 14:38:19 +0530

    First Commit

---
+++ Train Ticket Resservation/Passenger.py
@@ -0,0 +1,12 @@
+class Passenger:
+    id=0
+    name=''
+    age=0
+    berth=''
+    confirmBerth=None
+    seatNo=0
+    def __init__(self,id,name,age,berth):
+        self.id=id
+        self.name=name
+        self.age=age
+        self.berth=berth
